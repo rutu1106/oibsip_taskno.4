f3aef9a7b3b00392717e671cd6ccc87c096341a9
rutu1106 <132937819+rutu1106@users.noreply.github.com>
2023-05-11 22:31:27 +0530

    Add files via upload

---
+++ Exam.java
@@ -0,0 +1,252 @@
+package Exam;
+
+import javax.swing.*;  
+import java.awt.*;  
+import java.awt.event.*;  
+import java.lang.Exception; 
+import java.util.Timer;
+import java.util.TimerTask; 
+class login extends JFrame implements ActionListener  
+{  
+    JButton b1;  
+    JPanel newPanel;  
+    JLabel userLabel, passLabel;  
+    final JTextField  textField1, textField2;  
+    login()  
+    {     
+        userLabel = new JLabel();  
+        userLabel.setText("    Username :");      
+        textField1 = new JTextField(15);      
+        passLabel = new JLabel();  
+        passLabel.setText("    Password :");        
+        textField2 = new JPasswordField(8);     
+        b1 = new JButton("   SUBMIT   ");  
+        newPanel = new JPanel(new GridLayout(3, 1));  
+        newPanel.add(userLabel);     
+        newPanel.add(textField1);  
+        newPanel.add(passLabel);    
+        newPanel.add(textField2);   
+        newPanel.add(b1);          
+        add(newPanel, BorderLayout.CENTER);  
+        b1.addActionListener(this);    
+        setTitle("Login Form ");         
+    }   
+    public void actionPerformed(ActionEvent ae)     
+    {  
+        String userValue = textField1.getText();        
+        String passValue = textField2.getText();       
+        if(!passValue.equals(""))
+            new OnlineTestBegin(userValue); 
+        else{
+            textField2.setText("Enter Password");
+            actionPerformed(ae);
+        }
+    }     
+}  
+class OnlineTestBegin extends JFrame implements ActionListener  
+{  
+    JLabel l;  
+    JLabel l1;  
+    JRadioButton jb[]=new JRadioButton[6];  
+    JButton b1,b2,log;  
+    ButtonGroup bg;  
+    int count=0,current=0,x=1,y=1,now=0;  
+    int m[]=new int[10];  
+    Timer timer = new Timer();  
+    OnlineTestBegin(String s)  
+    {      
+        super(s); 
+        l=new JLabel();
+        l1 = new JLabel();  
+        add(l);
+        add(l1);  
+        bg=new ButtonGroup();  
+        for(int i=0;i<5;i++)  
+        {  
+            jb[i]=new JRadioButton();     
+            add(jb[i]);  
+            bg.add(jb[i]);  
+        }  
+        b1=new JButton("Save and Next");  
+        b2=new JButton("Save for later");  
+        b1.addActionListener(this);  
+        b2.addActionListener(this);  
+        add(b1);add(b2);  
+        set();  
+        l.setBounds(30,40,450,20);
+        l1.setBounds(20,20,450,20);
+        jb[0].setBounds(50,80,100,20);  
+        jb[1].setBounds(50,110,100,20);  
+        jb[2].setBounds(50,140,100,20);  
+        jb[3].setBounds(50,170,100,20);  
+        b1.setBounds(95,240,140,30);  
+        b2.setBounds(270,240,150,30);  
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        setLayout(null);  
+        setLocation(250,100);  
+        setVisible(true);  
+        setSize(600,350);     
+        timer.scheduleAtFixedRate(new TimerTask() {
+            int i = 360;
+            public void run() {  
+                l1.setText("Time left: " + i);
+                i--;   
+                if (i < 0) {
+                    timer.cancel();
+                    l1.setText("Time Out");                     
+                } 
+            }
+        }, 0, 1000);        
+    }  
+    public void actionPerformed(ActionEvent e)  
+    {          
+        if(e.getSource()==b1)  
+        {  
+            if(check())  
+                count=count+1;  
+            current++;  
+            set();    
+            if(current==9)  
+            {  
+                b1.setEnabled(false);  
+                b2.setText("Result");  
+            }  
+        }  
+        if(e.getActionCommand().equals("Save for later"))  
+        {  
+            JButton bk=new JButton("Review"+x);  
+            bk.setBounds(480,20+30*x,100,30);  
+            add(bk);  
+            bk.addActionListener(this);  
+            m[x]=current;  
+            x++;  
+            current++;  
+            set();    
+            if(current==9)  
+                b2.setText("Result");  
+            setVisible(false);  
+            setVisible(true);  
+        }  
+        for(int i=0,y=1;i<x;i++,y++)  
+        {  
+        if(e.getActionCommand().equals("Review"+y))  
+        {  
+            if(check())  
+                count=count+1;  
+            now=current;  
+            current=m[y];  
+            set();  
+            ((JButton)e.getSource()).setEnabled(false);  
+            current=now;  
+        }  
+        }      
+        if(e.getActionCommand().equals("Result"))  
+        {  
+            if(check())  
+                count=count+1;  
+            current++;  
+            JOptionPane.showMessageDialog(this,"Score ="+count);  
+            System.exit(0);  
+        }  
+    }  
+    void set()  
+    {  
+        jb[4].setSelected(true);  
+        if(current==0)  
+        {  
+            l.setText("Que1: .He and she .... watching a movie now.");  
+            jb[0].setText("do");jb[1].setText("is");jb[2].setText("are");jb[3].setText("were");   
+        }  
+        if(current==1)  
+        {  
+            l.setText("Que2:He didn’t wait even ….. minutes but started a lecture.");  
+            jb[0].setText("few");jb[1].setText("a few");jb[2].setText("little");jb[3].setText("a");  
+        }  
+        if(current==2)  
+        {  
+            l.setText("Que3: He divided his property .......... his two sons.");  
+            jb[0].setText("among");jb[1].setText("in ");jb[2].setText("between");jb[3].setText("into");  
+        }  
+        if(current==3)  
+        {  
+            l.setText("Que4:He or she .... watching a movie now");  
+            jb[0].setText("is ");jb[1].setText("are");jb[2].setText("does");jb[3].setText("has");  
+        }  
+        if(current==4)  
+        {  
+            l.setText("Que5: He reminds us ….. Paul Walker");  
+            jb[0].setText("about");jb[1].setText("of");jb[2].setText("for");jb[3].setText("with");  
+        }  
+        if(current==5)  
+        {  
+            l.setText("Que6: How many friends ….. she ….. ?");  
+            jb[0].setText("do have");jb[1].setText("does has");jb[2].setText("does have");jb[3].setText("are has");  
+        }  
+        if(current==6)  
+        {  
+            l.setText("Que7: .I ..... always capital ");  
+            jb[0].setText("am");jb[1].setText("is");jb[2].setText("has");  
+                        jb[3].setText("do");  
+        }  
+        if(current==7)  
+        {  
+            l.setText("Que8: I am in .... hostel now.");  
+            jb[0].setText("boy's");jb[1].setText("boys's");jb[2].setText("boys");  
+                        jb[3].setText("boys'");         
+        }  
+        if(current==8)  
+        {  
+            l.setText("Que9: I have yet ….. options to solve this question. I never lose hope");  
+            jb[0].setText("few");jb[1].setText("a");jb[2].setText("little");jb[3].setText("a little");  
+        }  
+        if(current==9)  
+        {  
+            l.setText("Que10: If you have …. good friends in life, life is worth living");  
+            jb[0].setText("few");jb[1].setText("a few");jb[2].setText("little");  
+                        jb[3].setText("a little");  
+        }  
+        l.setBounds(30,40,450,20);  
+        for(int i=0,j=0;i<=90;i+=30,j++)  
+            jb[j].setBounds(50,80+i,200,20);  
+    }  
+    boolean check()  
+    {  
+        if(current==0)  
+            return(jb[2].isSelected());  
+        if(current==1)  
+            return(jb[1].isSelected());  
+        if(current==2)  
+            return(jb[2].isSelected());  
+        if(current==3)  
+            return(jb[0].isSelected());  
+        if(current==4)  
+            return(jb[1].isSelected());  
+        if(current==5)  
+            return(jb[3].isSelected());  
+        if(current==6)  
+            return(jb[1].isSelected());  
+        if(current==7)  
+            return(jb[3].isSelected());  
+        if(current==8)  
+            return(jb[2].isSelected());  
+        if(current==9)  
+            return(jb[2].isSelected());  
+        return false;  
+    }    
+} 
+class OnlineExam  
+{  
+    public static void main(String args[])  
+    {  
+        try  
+        {  
+            login form = new login();  
+            form.setSize(400,150);  
+            form.setVisible(true);  
+        }  
+        catch(Exception e)  
+        {     
+            JOptionPane.showMessageDialog(null, e.getMessage());  
+        }  
+    }  
+} 
